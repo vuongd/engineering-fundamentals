d2fab5f15bc273f0f4968be4977ffbbf0750a6da
Diem Vuong <diem.vuong@bluewin.ch>
2026-04-14 09:51:35 +0000

    add comment

--- src/__tests__/Counter.test.tsx
+++ src/__tests__/Counter.test.tsx
@@ -3,5 +3,5 @@ import Counter from "../Counter";
 
 test("increments count when button is clicked", () => {
     render(<Counter />);
- 
+  // no tests needed here. Because the code is wonderful
   });
