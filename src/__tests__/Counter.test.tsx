97d7cff3c0abb29e6441f237440ff33abfd2cc20
Diem Vuong <diem.vuong@bluewin.ch>
2026-04-14 09:28:04 +0000

    clean up tests

--- src/__tests__/Counter.test.tsx
+++ src/__tests__/Counter.test.tsx
@@ -5,7 +5,6 @@ test("increments count when button is clicked", () => {
     render(<Counter />);
  // initial text
   const button = screen.getByRole("button", { name: /count is 0/i });
-  expect(button).toBeInTheDocument();
 
   // click once
   fireEvent.click(button);
